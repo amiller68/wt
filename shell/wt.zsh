e05f55dfc7085484daeb78037aa42d7560f3c2e7
al <alex@getquotient.ai>
2026-01-06 19:00:40 -0500

    Add ~/.local/bin to PATH in shell files

--- shell/wt.zsh
+++ shell/wt.zsh
@@ -1,6 +1,9 @@
 # wt - git worktree manager
 # https://github.com/amiller68/worktree
 
+# Ensure ~/.local/bin is in PATH
+[[ ":$PATH:" != *":$HOME/.local/bin:"* ]] && export PATH="$HOME/.local/bin:$PATH"
+
 wt() {
     if [[ "$1" == "open" || "$1" == "-o" ]]; then
         eval "$(_wt "$@")"
