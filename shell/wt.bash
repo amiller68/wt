895dc8d887a2c670d5eecc2acff0688e1d316c0e
al <alex@getquotient.ai>
2026-01-06 19:09:52 -0500

    Add tab completion for zsh and bash

--- shell/wt.bash
+++ shell/wt.bash
@@ -11,3 +11,29 @@ wt() {
         _wt "$@"
     fi
 }
+
+# Completion
+_wt_complete() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+
+    if [[ $COMP_CWORD -eq 1 ]]; then
+        COMPREPLY=($(compgen -W "create list remove open cleanup update version -o" -- "$cur"))
+    elif [[ $COMP_CWORD -eq 2 ]]; then
+        case $prev in
+            open|remove)
+                local repo=$(git rev-parse --show-toplevel 2>/dev/null)
+                if [[ -d "$repo/.worktrees" ]]; then
+                    COMPREPLY=($(compgen -W "$(ls "$repo/.worktrees" 2>/dev/null)" -- "$cur"))
+                fi
+                ;;
+            update)
+                COMPREPLY=($(compgen -W "--force" -- "$cur"))
+                ;;
+            -o)
+                COMPREPLY=($(compgen -W "create" -- "$cur"))
+                ;;
+        esac
+    fi
+}
+complete -F _wt_complete wt
--- shell/wt.zsh
+++ shell/wt.zsh
@@ -11,3 +11,40 @@ wt() {
         _wt "$@"
     fi
 }
+
+# Completion
+_wt_completion() {
+    local -a commands
+    commands=(
+        'create:Create a new worktree'
+        'list:List all worktrees'
+        'remove:Remove a worktree'
+        'open:cd to worktree directory'
+        'cleanup:Remove all worktrees'
+        'update:Update wt to latest version'
+        'version:Show version info'
+    )
+
+    if (( CURRENT == 2 )); then
+        _describe -t commands 'wt commands' commands
+        compadd -- '-o'
+    elif (( CURRENT == 3 )); then
+        case ${words[2]} in
+            open|remove)
+                local repo=$(git rev-parse --show-toplevel 2>/dev/null)
+                if [[ -d "$repo/.worktrees" ]]; then
+                    local -a worktrees
+                    worktrees=($(ls "$repo/.worktrees" 2>/dev/null))
+                    _describe -t worktrees 'worktrees' worktrees
+                fi
+                ;;
+            update)
+                compadd -- '--force'
+                ;;
+            -o)
+                compadd -- 'create'
+                ;;
+        esac
+    fi
+}
+compdef _wt_completion wt
